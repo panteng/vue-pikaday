b10de6a8913b1183154015c442088873ea94eddb
Chris Pan <chrispan.pt@gmail.com>
2017-04-29 09:40:34 +0800

    Update webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,46 @@
+const path = require('path');
+
 module.exports = {
     entry: [
         './main.js'
     ],
     output: {
-        path: './bundles',
+        path: path.resolve(__dirname, 'bundles'),
         publicPath: '/bundles/',
         filename: 'bundle.js'
     },
     module: {
-        preLoaders: [
+        rules: [
             {
                 test: /\.js$/,
+                enforce: 'pre',
                 exclude: /node_modules/,
-                loader: 'jshint-loader'
-            }
-        ],
-        loaders: [
-            {
-                test: /\.html$/, loader: "html"
+                use: [
+                    {
+                        loader: 'jshint-loader',
+                        options: {
+                            emitErrors: false,
+                            failOnHint: false
+                        }
+                    }
+                ]
             },
             {
-                test: /\.css$/, loader: "style!css"
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
             }
         ]
-    },
-    jshint: {
-        emitErrors: false,
-        failOnHint: false
     },
     devServer: {
         contentBase: './'
